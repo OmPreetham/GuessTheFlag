239dc7a9149228dbe97ecbc89308366988ce2f84
Om Preetham <ompreethambandi@gmail.com>
2024-06-25 23:32:15 -0500

    enhanced with accessibility

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -32,6 +32,21 @@ struct ContentView: View {
     @State private var scoreTitle = ""
     
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes. Top stripe blue, middle stripe black, bottom stripe white.",
+        "France": "Flag with three vertical stripes. Left stripe blue, middle stripe white, right stripe red.",
+        "Germany": "Flag with three horizontal stripes. Top stripe black, middle stripe red, bottom stripe gold.",
+        "Ireland": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe orange.",
+        "Italy": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe red.",
+        "Nigeria": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe green.",
+        "Poland": "Flag with two horizontal stripes. Top stripe white, bottom stripe red.",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red.",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background.",
+        "Ukraine": "Flag with two horizontal stripes. Top stripe blue, bottom stripe yellow.",
+        "US": "Flag with many red and white stripes, with white stars on a blue background in the top-left corner."
+    ]
+
     @State private var correctAnswer = Int.random(in: 0...2)
     
     @State private var score = 0
@@ -82,6 +97,7 @@ struct ContentView: View {
                         } label: {
                             FlagImageView(imageName: countries[number], rotation: rotationAmounts[number], opacity: opacityAmounts[number], scale: scaleAmounts[number])
                         }
+                        .accessibilityLabel(labels[countries[number], default: "Unknown flag"])
                     }
                     .alert(scoreTitle, isPresented: $showingScore) {
                         Button("Continue", action: askQuestion)
